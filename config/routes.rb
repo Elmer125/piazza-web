9ed3056d4ce31ccb7f67e6ec18ce74cb032af949
Elmer125 <elmermauricio05@hotmail.com>
2024-01-31 19:51:23 -0500

    Complete user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,16 @@ Rails.application.routes.draw do
   get "sign_up", to: "users#new"
   post "sign_up", to: "users#create"
 
+  #Session routes
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
+
+  # TODO:
+  resource :profile, only: [:show, :update], controller: "users"
+
+  namespace :users do
+    patch "change_password", to: "passwords#update"
+  end
+
 end
