2b2ff72deac186b0f9a3a33ef14b62ceacdd0f05
Elmer125 <elmermauricio05@hotmail.com>
2024-02-22 20:37:54 -0500

    Native actions in web bridge

--- app/javascript/bridge/bridge_element.js
+++ app/javascript/bridge/bridge_element.js
@@ -15,10 +15,15 @@ export default class BridgeElement {
     return this.element.getAttribute(`data-bridge-element-${this.platform}`);
   }
 
+  get id(){
+    return this.element.dataset.bridgeElementId
+  }
+
   toMessage(){
     return{
       type: "render",
       data:{
+        id: this.id,
         title: this.title,
         ...JSON.parse(this.platformData)
       }
