9d93c1ce8264f4af31eb05a906e7ab33ebf2ef91
Elmer125 <elmermauricio05@hotmail.com>
2025-02-11 20:39:07 -0500

    Prepare the server for Turbo Native apps

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -10,4 +10,16 @@ class ApplicationHelperTest < ActionView::TestCase
   test "returns app name when the page title is missing" do
     assert_equal I18n.t('piazza'), title
   end
+
+  test "only page specific title is return for turbo native" do
+    @turbo_native_app = true
+    content_for(:title) { "Page Title" }
+
+    assert_equal "Page Title", title
+  end
+
+  private
+  def turbo_native_app?
+    @turbo_native_app
+  end
 end
